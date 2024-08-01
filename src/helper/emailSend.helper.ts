171b9ecb1535471e092b2e42a97065b46b30928a
kachhadiyaSachin <sachin.kachhadiya@bigbrainyinfotech.com>
2024-08-01 18:02:23 +0530

    fix

--- src/helper/emailSend.helper.ts
+++ src/helper/emailSend.helper.ts
@@ -7,7 +7,7 @@ export const sendEMAIL = async (email:any,subject:any,text:any,message:any) => {
     const transporter = nodemailer.createTransport({
         service: "gmail",
         host: "smtp.gmail.com",
-        port: 587,
+        port: 465,
         secure: true,
         auth: {
           user: process.env.EMAIL_USER,
